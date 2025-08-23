836a275ed0496ba5700e17f2d0a1e95c7ef9cfba
Bayanda Jack <2669074@students.wits.ac.za>
2025-08-23 20:32:37 +0200

    feat(files): Add all files needed for project so far

---
+++ buddy.c
@@ -0,0 +1,113 @@
+
+#include "buddy.h"
+#include <sys/mman.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+//Define your Constants here
+
+
+
+
+
+enum AEflag { Free, Taken };
+
+struct head {
+	enum AEflag status;
+	short int level;
+	struct head* next;
+	struct head* prev;
+};
+
+
+//Complete the implementation of new here
+struct head *new() {
+	
+
+
+
+
+
+}
+
+//Complete the implementation of level here
+int level(int req) {
+
+
+
+
+}
+
+//Complete the implementation of balloc here
+void* balloc(size_t size) {
+	
+
+
+
+}
+
+//Complete the implementation of bfree here
+void bfree(void* memory) {
+	
+
+
+
+
+}
+
+
+
+//Helper Functions
+struct head* buddy(struct head* block) {
+	int index = block->level;
+	long int mask = 0x1 << (index + MIN);
+	return (struct head*)((long int)block ^ mask);
+
+}
+
+struct head* split(struct head* block) {
+	int index = block->level - 1;
+	int mask = 0x1 << (index + MIN);
+	return (struct head*)((long int)block | mask);
+}
+
+struct head* primary(struct head* block) {
+	int index = block->level;
+	long int mask = 0xffffffffffffffff << (1 + index + MIN);
+	return (struct head*)((long int)block & mask);
+}
+
+void* hide(struct head* block) {
+	return (void*)(block + 1);
+}
+
+
+struct head* magic(void* memory) {
+	return ((struct head*)memory - 1);
+}
+	
+
+
+void dispblocklevel(struct head* block){
+	printf("block level = %d\n",block->level);
+}
+void dispblockstatus(struct head* block){
+	printf("block status = %d\n",block->status);
+}
+
+void blockinfo(struct head* block){
+	printf("===================================================================\n");
+	dispblockstatus(block);
+	dispblocklevel(block);
+	printf("start of block in memory: %p\n", block);
+	printf("size of block in memory: %ld in bytes\n",sizeof(struct head));
+	printf("===================================================================\n");
+}
+
+
+
+
+
+
